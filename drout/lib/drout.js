99f5b4c6a893cc77b37135c6fcd6a9577f50eaf9
graphicsforge <graphicsforge@gmail.com>
2019-09-25 18:34:14 -0400

    editor WiP

--- drout/lib/drout.js
+++ drout/lib/drout.js
@@ -1,26 +1,39 @@
 const React = require("react");
 const ReactDom = require("react-dom");
 
-module.exports = {
-  render: (options) => {
+const getComponent = (options) => {
+  if (!options.component && !options.componentId) return null;
+  let component = options.component;
+  if (!component) {
+    const componentId = options.componentId;
+    try {
+      component = require(componentId);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+  return component;
+}
+
+class Drout {
+  static render(options) {
     if (!options) {
       console.warn("drout renderer called on null");
       return null;
     }
-    if (!options.component && !options.componentId) {
-      return JSON.stringify(options);
-    }
-    let component = options.component;
-    if (!component) {
-      const componentId = options.componentId;
-      try {
-        component = require(componentId);
-      } catch(err) {
-        console.error(err);
-      }
-    }
+    const component = getComponent(options);
+    if (!component) return JSON.stringify(options);
     return component.render(options);
-  },
-  React,
-  ReactDom,
-};
+  }
+  static getDefault(options) {
+    const component = getComponent(options);
+    if (component && component.getDefault) {
+      return component.getDefault(options);
+    }
+    return null;
+  }
+}
+Drout.React = React;
+Drout.ReactDom = ReactDom;
+
+module.exports = Drout;
